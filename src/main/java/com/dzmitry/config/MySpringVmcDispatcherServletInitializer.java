8566eb124b96362a3cb1a32578f04c851becc373
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-09-19 10:39:35 +0200

    init rest Hello world!

---
+++ src/main/java/com/dzmitry/config/MySpringVmcDispatcherServletInitializer.java
@@ -0,0 +1,21 @@
+package com.dzmitry.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+public class MySpringVmcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class[0];
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class[] {DemoAppConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[] {"/"};
+    }
+}
