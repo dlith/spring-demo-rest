9911852c6302b1ec37edafdadf0fb5005770f755
Dzmitry Liashuk <dlyashuk1997@gmail.com>
2022-09-19 12:10:57 +0200

    pathVariable for rest endpoints

--- src/main/java/com/dzmitry/rest/StudentRestController.java
+++ src/main/java/com/dzmitry/rest/StudentRestController.java
@@ -2,9 +2,11 @@ package com.dzmitry.rest;
 
 import com.dzmitry.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -12,14 +14,23 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> students(){
+    private List<Student> students;
 
-        List<Student> students = new ArrayList<>();
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
         students.add(new Student("John", "Snow"));
         students.add(new Student("Kaitlin", "Stark"));
         students.add(new Student("Mary", "Stark"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> students(){
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student student(@PathVariable int studentId){
+        return students.get(studentId);
     }
 }
